bf7d9e50e905d25e23eccc3b8f33a8da23a0f1c3
Minh Hoàng <52208309+mHoang99@users.noreply.github.com>
2021-04-07 13:23:07 +0700

    lab04

--- Lab04/UserSorting.php
+++ Lab04/UserSorting.php
@@ -1,104 +1,128 @@
 <?php
-
-function user_sort($a, $b) {
-
-// smarts is all-important, so sort it first
-
-    if($b == 'smarts') {
-
+function user_sort($a, $b)
+{
+    // smarts is all-important, so sort it first
+    if ($b == 'smarts') {
         return 1;
-
-    }
-
-    else if($a == 'smarts') {
-
+    } else if ($a == 'smarts') {
         return -1;
-
     }
 
     return ($a == $b) ? 0 : (($a < $b) ? -1 : 1);
-
 }
 
-$values = array('name' => 'Buzz Lightyear',
+$submitted_click = 0;
+//$sort_type = null;
+//$submitted = null;
 
+$values = array(
+    'name' => 'Buzz Lightyear',
     'email_address' => 'buzz@starcommand.gal',
-
     'age' => 32,
+    'smarts' => 'some'
+);
+if (isset($_POST['sort_type'])) $sort_type = $_POST['sort_type'];
+$sort_config = ['usort' => ['usort', 'user_sort'],
+    'uksort' => ['uksort', 'user_sort'],
+    'uasort' => ['uasort', 'user_sort'],
+    'sort' => ['sort'],
+    'rsort' => ['rsort'],
+    'ksort' => ['ksort'],
+    'krsort' => ['krsort'],
+    'asort' => ['asort'],
+    'arsort' => ['arsort']];
+if (isset($_POST['submitted'])) $submitted = $_POST['submitted'];
+else $submitted = null;
 
-    'smarts' => 'some');
+$sorted_values = array_merge([], $values);
 
-$submitted = $_GET['submitted'];
-$sort_type = $_GET['sort_type'];
-
-if($submitted) {
-
-    if($sort_type == 'usort' || $sort_type == 'uksort' || $sort_type == 'uasort') {
-
-        $sort_type($values, 'user_sort');
-
+if ($submitted) {
+    $config = $sort_config[$sort_type];
+    if (isset($config[1])) {
+        $config[0]($sorted_values, $config[1]);
+    } else {
+        $config[0]($sorted_values);
     }
-
-    else {
-
-        $sort_type($values);
-
-    }
-
+    $submitted_click = 1;
 }
-
 ?>
 
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="get">
-
-    <p>
-
-        <input type="radio" name="sort_type" value="sort" checked="checked" />
-
-        Standard sort<br />
-
-        <input type="radio" name="sort_type" value="rsort" /> Reverse sort<br />
-
-        <input type="radio" name="sort_type" value="usort" /> User-defined sort<br />
-
-        <input type="radio" name="sort_type" value="ksort" /> Key sort<br />
-
-        <input type="radio" name="sort_type" value="krsort" /> Reverse key sort<br />
-
-        <input type="radio" name="sort_type" value="uksort" /> User-defined key sort<br />
-
-        <input type="radio" name="sort_type" value="asort" /> Value sort<br />
-
-        <input type="radio" name="sort_type" value="arsort" /> Reverse value sort<br />
-
-        <input type="radio" name="sort_type" value="uasort" /> User-defined value sort<br />
-
-    </p>
-
+<form action="UserSorting.php" method="post">
+    <table align="center" style="width:120%">
+        <tbody>
+        <tr>
+            <td rowspan="3">
+                <input type="radio" name="sort_type" value="sort" checked="checked"/>
+                Standard sort<br/>
+            </td>
+            <td rowspan="3">
+                <input type="radio" name="sort_type" value="usort"/> User-defined sort<br/>
+            </td>
+            <td rowspan="3">
+                <input type="radio" name="sort_type" value="krsort"/> Reverse key sort<br/>
+            </td>
+            <td rowspan="2">
+                <input type="radio" name="sort_type" value="asort"/> Value sort<br/>
+            </td>
+        </tr>
+        <tr></tr>
+        <tr>
+            <td rowspan="2">
+                <input type="radio" name="sort_type" value="arsort"/> Reverse value sort<br/>
+            </td>
+        </tr>
+        <tr>
+            <td rowspan="3">
+                <input type="radio" name="sort_type" value="rsort"/> Reverse sort<br/>
+            </td>
+            <td rowspan="3">
+                <input type="radio" name="sort_type" value="ksort"/> Key sort<br/>
+            </td>
+            <td rowspan="3">
+                <input type="radio" name="sort_type" value="uksort"/> User-defined key sort<br/>
+            </td>
+        </tr>
+        <tr>
+            <td rowspan="2">
+                <input type="radio" name="sort_type" value="uasort"/> User-defined value sort<br/>
+            </td>
+        </tr>
+        </tbody>
+    </table>
     <p align="center">
-
-        <input type="submit" value="Sort" name="submitted" />
-
+        <input type="submit" value="Sort" name="submitted"/>
     </p>
-
-    <p>
-
-        Values <?= $submitted ? "sorted by $sort_type" : "unsorted"; ?>:
-
-    </p>
-
-    <ul>
-
-        <?php
-
-        foreach($values as $key=>$value) {
-
-            echo "<li><b>$key</b>: $value</li>";
-
-        }
-
-        ?>
-
-    </ul>
-
+    <table style="width:100%">
+        <tbody>
+        <tr>
+            <td style="width:15%" text-align="center">
+                <p>
+                    Values unsorted (before sort):
+                </p>
+            </td>
+            <td>
+                <ul>
+                    <?php
+                    foreach ($values as $key => $value) {
+                        echo "<li><b>$key</b>: $value</li>";
+                    }
+                    ?>
+                </ul>
+            </td>
+            <?php
+            if ($submitted_click == 1) {
+                print "<td>";
+                print "<p>Values sorted by $sort_type:</p>";
+                print "</td><td>";
+                print "<ul>";
+                foreach ($sorted_values as $key => $value) {
+                    echo "<li><b>$key</b>: $value</li>";
+                }
+                print "</ul>";
+                print "</td>";
+            }
+            ?>
+        </tr>
+        </tbody>
+    </table>
 </form>
