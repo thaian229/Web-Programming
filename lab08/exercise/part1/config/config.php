19db7d8f18d2f09353b646432865aae47090c350
Minh Hoàng <52208309+mHoang99@users.noreply.github.com>
2021-05-18 18:59:47 +0700

    Lab08

---
+++ lab08/exercise/part1/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+/** Configuration Variables **/
+
+define ('DEVELOPMENT_ENVIRONMENT',true);
+
+define('DB_NAME', 'lab8.1');
+define('DB_USER', 'thaian229');
+define('DB_PASSWORD', '22114455');
+define('DB_HOST', 'localhost');
---
+++ lab08/exercise/part1/public/index.php
@@ -0,0 +1,13 @@
+<?php	
+
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(dirname(__FILE__)));
+
+if (isset($_GET['url']))
+{
+    $url = $_GET['url'];
+}
+
+require_once (ROOT . DS . 'library' . DS . 'bootstrap.php');
+
+?>
