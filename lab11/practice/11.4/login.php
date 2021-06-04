c8037a561743df2274dc8c5f1c709b866099eea5
Nguyễn Thái An <thaian229@gmail.com>
2021-06-04 13:46:27 +0700

    add lab 11

---
+++ lab11/practice/11.4/login.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+    <title>Login</title>
+</head>
+
+<body>
+<form action="check_login.php" method="post">
+    <h1>Login form</h1>
+    <br>
+    Username: <input type="text" name="username"><br/>>
+    Password: <input type="password" name="password"><br/>
+    <input type='submit' value='Log in'>
+</form>
+</body>
+</html>
---
+++ lab11/practice/11.4/prefs-demo-session.php
@@ -0,0 +1,19 @@
+<html>
+<?php
+session_start();
+// $bg = $_SESSION['bg'];
+// $fg = $_SESSION['fg'];
+$name = $_SESSION['name'];
+?>
+
+
+<body>
+<h1>Welcome <?=$name?> to the Store</h1>
+
+We have many fine products for you to view.<br>
+Please feel free to browse the aisles and stop an assistant at any time.<br>
+But remember, you break it, you bought it!
+
+</body>
+
+</html>
